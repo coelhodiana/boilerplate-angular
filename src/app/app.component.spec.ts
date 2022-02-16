2ffddc864267f0171e2064c3cd273a3dc6f144c7
Natália Luiza <natalialuizas@live.com>
2022-02-16 17:54:35 -0300

    build(pre-commit): removing flag test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('boilerplate-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('boilerplatea-angular app is running!');
   });
 });
